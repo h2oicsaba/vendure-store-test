d06d3337f5e2672f040eacccb03f2cd0eeca11e0
h2oicsaba <csaba.vizi@gmail.com>
2025-06-09 13:31:20 +0200

    fix: add empty patch file to prevent MODULE_NOT_FOUND errors

---
+++ src/patch-express-rate-limit.js
@@ -0,0 +1,9 @@
+/**
+ * Empty patch file to prevent MODULE_NOT_FOUND errors
+ * This file does nothing, it just exists to satisfy any potential imports
+ */
+
+console.log('Empty patch file loaded - no actual patching performed');
+
+// Export an empty object
+module.exports = {};
