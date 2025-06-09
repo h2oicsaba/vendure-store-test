da4bf761aef4d552554566ad3ff9a77760755663
h2oicsaba <csaba.vizi@gmail.com>
2025-06-09 14:45:10 +0200

    fix: use EXPRESS_RATE_LIMIT_TRUST_PROXY env var

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import { bootstrap, runMigrations } from '@vendure/core';
 import { config } from './vendure-config';
 
 // Set environment variables for express-rate-limit
-process.env.TRUST_PROXY = 'true';
+process.env.EXPRESS_RATE_LIMIT_TRUST_PROXY = 'true';
 process.env.EXPRESS_DISABLE_RATE_LIMIT = 'true';
 
 // Log environment variables for debugging
 console.log('Environment variables set for express-rate-limit:');
-console.log('TRUST_PROXY:', process.env.TRUST_PROXY);
+console.log('EXPRESS_RATE_LIMIT_TRUST_PROXY:', process.env.EXPRESS_RATE_LIMIT_TRUST_PROXY);
 console.log('EXPRESS_DISABLE_RATE_LIMIT:', process.env.EXPRESS_DISABLE_RATE_LIMIT);
 
 runMigrations(config)
